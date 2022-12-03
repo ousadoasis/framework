51824ca89be4226cfb358a9438c1f1aa98e600a4
director <director@ousadoasis.com>
2022-12-03 17:33:51 +0000

    Make trade dashboard for site, i.e. live trades, or trade history

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+const Binance = require('node-binance-api');
+require('dotenv').config()
+
+
 /**
- * Volume Weighted Average Price (VWAP)
+ * Get trading dashboard
  */
+const binance = new Binance().options({
+  APIKEY: process.env.binanceAPIKEY,
+  APISECRET: process.env.BINANCEAPISECRET
+});
 
-
+binance.trades("BTCBUSD", (error, trades, symbol) => {
+  console.info(symbol+" trade history", trades);
+});
