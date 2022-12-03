a1ff433eeb6236587ed96473bafab2954394216b
director <director@ousadoasis.com>
2022-12-03 18:23:58 +0000

    tidy up trades i.e. delete trade-history for today if day has not expired yet

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const jsonexport = require('jsonexport');
 const fs = require('fs');
 require('dotenv').config()
 
+const toDateDashed = (d) => d.getDate() + '-' + (d.getMonth()+1) + '-' + d.getFullYear();
 
 
 /**
@@ -16,13 +17,18 @@ const binance = new Binance().options({
 binance.trades("BTCBUSD", (error, trades, symbol) => {
   console.info(symbol+" trade history", trades);
   const runningTotals = {}
+
+  // If trades are for today, then delete the file as likely out of date
+  const todayDate = toDateDashed(new Date());
+  fs.rmSync(`trades-${todayDate}.csv`);
+
   
   trades.forEach((v) => { 
     var d = new Date(v.time);
-    const tidyDate = d.getDate() + '-' + (d.getMonth()+1) + '-' + d.getFullYear();
-    if(v.isBuyer){
-      runningTotals[tidyDate] = (runningTotals[tidyDate] ?? 0) + parseInt(v.quoteQty);
-    }
+    const tidyDate = toDateDashed(d);
+    //if(v.isBuyer){
+    runningTotals[tidyDate] = ((runningTotals[tidyDate] ?? 0) + parseFloat(v.quoteQty)) ?? 0
+    //}
     fs.appendFileSync(`trades-${tidyDate}.csv`, JSON.stringify({...v, tidyDate }) + '\n');
   });
   console.log('runningTotals', runningTotals);
