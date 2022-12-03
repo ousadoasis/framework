93365781de3e21d21e59b9740121882fc70ed193
director <director@ousadoasis.com>
2022-12-03 18:01:49 +0000

    output daily trading activity

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const Binance = require('node-binance-api');
+const jsonexport = require('jsonexport');
+const fs = require('fs');
 require('dotenv').config()
+
 
 
 /**
@@ -12,4 +15,16 @@ const binance = new Binance().options({
 
 binance.trades("BTCBUSD", (error, trades, symbol) => {
   console.info(symbol+" trade history", trades);
-});
+  const runningTotals = {}
+  
+  trades.forEach((v) => { 
+    var d = new Date(v.time);
+    const tidyDate = d.getDate() + '-' + (d.getMonth()+1) + '-' + d.getFullYear();
+    if(v.isBuyer){
+      runningTotals[tidyDate] = (runningTotals[tidyDate] ?? 0) + parseInt(v.quoteQty);
+    }
+    fs.appendFileSync(`trades-${tidyDate}.csv`, JSON.stringify({...v, tidyDate }) + '\n');
+  });
+  console.log('runningTotals', runningTotals);
+  fs.writeFileSync('running-totals.json', JSON.stringify(runningTotals));
+})
